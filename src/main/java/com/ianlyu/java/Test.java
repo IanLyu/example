1724997c793713ea3532e846c956be9db481d088
Ian Lyu <ianlyu@163.com>
2018-11-25 21:27:12 +0800

    commit message

---
+++ src/main/java/com/ianlyu/java/Test.java
@@ -0,0 +1,12 @@
+package com.ianlyu.java;
+
+/**
+ * @Description 创建测试类
+ * @Author Ian Lyu
+ * @Date 2018/11/25 下午7:46
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
